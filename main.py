c8c07e5b58ce12bef02271e7e11d987315e07ffe
Abdullatif YILMAZ <latifyilmaza@gmail.com>
2021-03-13 10:45:42 +0300

    lottie files animations added

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ app.config['SECRET_KEY'] = "key"
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return (render_template('404.html'), 404)
